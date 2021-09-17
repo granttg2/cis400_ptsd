d7e4e50bb48a98a4d5e9b6dc81f7042513fefd82
Grant Gifford <granttg2@gmail.com>
2021-09-17 12:30:33 -0400

    Created Main Dashboard

---
+++ app/src/main/java/com/example/cis400_ptsd/CheckListMain.java
@@ -0,0 +1,6 @@
+package com.example.cis400_ptsd;
+
+import android.app.Activity;
+
+public class CheckListMain extends Activity {
+}
--- app/src/main/java/com/example/cis400_ptsd/MainActivity.java
+++ app/src/main/java/com/example/cis400_ptsd/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.cis400_ptsd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set our background to be a changing gradient
+        LinearLayout main_layout = findViewById(R.id.main_layout);
+        AnimationDrawable background_drawable = (AnimationDrawable) main_layout.getBackground();
+        background_drawable.setEnterFadeDuration(3000);
+        background_drawable.setExitFadeDuration(6000);
+        background_drawable.start();
+
+        //Instantiate buttons in our Main Activity
+        ImageButton reporting_button = findViewById(R.id.reporting_button);
+        ImageButton meditation_button = findViewById(R.id.spotify_button);
+        ImageButton game_button = findViewById(R.id.game_button);
+        ImageButton maps_button = findViewById(R.id.maps_button);
+
+        //Set the event handler for our buttons
+        reporting_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {switch_activities(CheckListMain.class);}});
+
+        meditation_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {switch_activities(GameMain.class);}});
+
+        game_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {switch_activities(GameMain.class);}});
+
+        maps_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {switch_activities(MapMain.class);}});
+
+    }
+
+    private void switch_activities(Class class_to_switch) {
+        Intent switchActivityIntent = new Intent(this, class_to_switch);
+        startActivity(switchActivityIntent);
     }
 }
