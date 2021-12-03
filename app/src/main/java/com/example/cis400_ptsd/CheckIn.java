d2d782d818ceaaa91778ee192da3b2a90c32a8c0
Grant Gifford <granttg2@gmail.com>
2021-12-03 13:30:27 -0500

    Complete graph and mood tracking

--- app/src/main/java/com/example/cis400_ptsd/CheckIn.java
+++ app/src/main/java/com/example/cis400_ptsd/CheckIn.java
@@ -1,5 +1,6 @@
 package com.example.cis400_ptsd;
 
+import android.content.Context;
 import android.graphics.Paint;
 import android.os.Bundle;
 
@@ -7,6 +8,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,9 +22,25 @@ import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 
 public class CheckIn extends Fragment {
@@ -65,8 +83,59 @@ public class CheckIn extends Fragment {
 
     }
 
-    private void recordMood(int mood){
+    private void recordMood(int recMood) {
+        LinkedHashMap<Date, Integer> moodObject = new LinkedHashMap<Date, Integer>();
+        try{
+            File check = new File(this.getContext().getFilesDir(), "mood");
+            if(check.exists()) {
 
+                FileInputStream file = this.getContext().openFileInput("mood");
+                InputStream buffer = new BufferedInputStream(file);
+                ObjectInput input = new ObjectInputStream(buffer);
+                try {
+                    moodObject = (LinkedHashMap<Date, Integer>) input.readObject();
+                } finally {
+                    input.close();
+                }
+            }else{
+                OutputStream file = this.getContext().openFileOutput("mood", Context.MODE_PRIVATE);
+                OutputStream buffer = new BufferedOutputStream(file);
+                ObjectOutput output = new ObjectOutputStream(buffer);
+                try{
+                    output.writeObject(moodObject);
+                }
+                finally{
+                    output.close();
+                }
+            }
+        }
+        catch(ClassNotFoundException ex){
+            Log.e("E", "Class not found");
+        }
+        catch(IOException ex){
+            Log.e("E", "IO Exception");
+        }
+
+        try{
+            OutputStream file = this.getContext().openFileOutput("mood", Context.MODE_PRIVATE);
+            OutputStream buffer = new BufferedOutputStream(file);
+            ObjectOutput output = new ObjectOutputStream(buffer);
+            try{
+                Date currDate = Calendar.getInstance().getTime();
+                if (moodObject.containsKey(currDate)){
+                    moodObject.remove(currDate);
+                }
+
+                moodObject.put(currDate, recMood);
+                output.writeObject(moodObject);
+            }
+            finally{
+                output.close();
+            }
+        }
+        catch(IOException ex){
+            Log.e("E", "IO Exception in recording 2");
+        }
 
         getActivity().findViewById(R.id.recycler).setVisibility(View.VISIBLE);
         getActivity().findViewById(R.id.fragmentView).setVisibility(View.GONE);
@@ -75,22 +144,55 @@ public class CheckIn extends Fragment {
 
 
     private void createGraph(View view){
-        ArrayList<Date> dates = new ArrayList();
+        LinkedHashMap<Date, Integer> moodObject = new LinkedHashMap<Date, Integer>();
+        try{
+            File check = new File(this.getContext().getFilesDir(), "mood");
+            if(check.exists()) {
+
+                FileInputStream file = this.getContext().openFileInput("mood");
+                InputStream buffer = new BufferedInputStream(file);
+                ObjectInput input = new ObjectInputStream(buffer);
+                try {
+                    moodObject = (LinkedHashMap<Date, Integer>) input.readObject();
+                } finally {
+                    input.close();
+                }
+            }else{
+                OutputStream file = this.getContext().openFileOutput("mood", Context.MODE_PRIVATE);
+                OutputStream buffer = new BufferedOutputStream(file);
+                ObjectOutput output = new ObjectOutputStream(buffer);
+                try{
+                    output.writeObject(moodObject);
+                }
+                finally{
+                    output.close();
+                }
+            }
+        }
+        catch(ClassNotFoundException ex){
+            Log.e("E", "Class not found");
+        }
+        catch(IOException ex){
+            Log.e("E", "IO Exception");
+        }
+
+        ArrayList<Date> dates = new ArrayList<Date>();
 
         Calendar calendar = Calendar.getInstance();
-        for (int i =0; i < 20; i++){
-            dates.add(calendar.getTime());
-            calendar.add(calendar.DATE, 1);
+        for (Date d: moodObject.keySet()){
+            dates.add(d);
         }
 
         DataPoint[] data = new DataPoint[dates.size()];
 
         for (int i =0; i < dates.size(); i++){
-            data[i] = new DataPoint(dates.get(i), Math.random() % 3);
+            Date d = dates.get(i);
+            data[i] = new DataPoint(d, moodObject.get(d));
         }
 
         GraphView graph = (GraphView) view.findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(data);
+        series.setDrawDataPoints(true);
 
         Paint paint = new Paint();
         paint.setStyle(Paint.Style.FILL);
@@ -104,8 +206,10 @@ public class CheckIn extends Fragment {
         graph.getGridLabelRenderer().setVerticalLabelsVisible(false);
         graph.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.NONE);
 
-        graph.getViewport().setMinX(dates.get(0).getTime());
-        graph.getViewport().setMaxX(dates.get(dates.size() - 1).getTime());
+        if(dates.size() > 0) {
+            graph.getViewport().setMinX(dates.get(0).getTime());
+            graph.getViewport().setMaxX(dates.get(dates.size() - 1).getTime());
+        }
         graph.getViewport().setXAxisBoundsManual(true);
         graph.getGridLabelRenderer().setHumanRounding(false);
     }
