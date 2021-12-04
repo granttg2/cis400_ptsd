36d567bda7d013f3aa92f410f7dcceae3ada7fd1
Timothy Nkata <timothynkata@Timothys-MacBook-Pro.local>
2021-12-04 14:00:57 -0500

    Meditation Update

--- app/src/main/java/com/example/cis400_ptsd/MeditationMain.java
+++ app/src/main/java/com/example/cis400_ptsd/MeditationMain.java
@@ -1,6 +1,46 @@
 package com.example.cis400_ptsd;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 
 public class MeditationMain extends Activity {
+    // Instantiating the MediaPlayer class
+    MediaPlayer music;
+
+    @Override
+    protected void onCreate(
+            Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Adding the music file to our
+        // newly created object music
+        music = MediaPlayer.create(
+                this, R.raw.sound);
+    }
+
+    // Playing the music
+    public void musicplay(View v)
+    {
+        music.start();
+    }
+
+    // Pausing the music
+    public void musicpause(View v)
+    {
+        music.pause();
+    }
+
+    // Stoping the music
+    public void musicstop(View v)
+    {
+        music.stop();
+        music
+                = MediaPlayer.create(
+                this, R.raw.sound);
+    }
 }
