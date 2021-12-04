bb67a1d6172098513b22a314374b7a9d49699848
jthoma25 <78917221+jthoma25@users.noreply.github.com>
2021-12-04 15:27:02 -0500

    Game implemented and tested. Added to manifest and added other resources and classes

--- app/src/main/java/com/example/cis400_ptsd/GameMain.java
+++ app/src/main/java/com/example/cis400_ptsd/GameMain.java
@@ -1,52 +1,65 @@
 package com.example.cis400_ptsd;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class GameMain extends Activity {
-    //method that creates activity instance
+public class GameMain extends AppCompatActivity {
+
+    private boolean isMute;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.game);
 
-        //Initialize the buttons from the main layout
-        Button game_1_button = findViewById(R.id.game_1_button);
-        Button game_2_button = findViewById(R.id.game_2_button);
-
-        //Set the event handlers for the buttons on the main page
-        game_1_button.setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                startActivity(new Intent(GameMain.this, Game1.class));
+            public void onClick(View view) {
+                startActivity(new Intent(GameMain.this, GameActivity.class));
             }
         });
 
-        game_2_button.setOnClickListener(new View.OnClickListener(){
+        TextView highScoreTxt = findViewById(R.id.highScoreTxt);
+
+        final SharedPreferences prefs = getSharedPreferences("game", MODE_PRIVATE);
+        highScoreTxt.setText("HighScore: " + prefs.getInt("highscore", 0));
+
+        isMute = prefs.getBoolean("isMute", false);
+
+        final ImageView volumeCtrl = findViewById(R.id.volumeCtrl);
+
+        if (isMute)
+            volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_off_24);
+        else
+            volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_up_24);
+
+        volumeCtrl.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                startActivity(new Intent(GameMain.this, Game2.class));
+            public void onClick(View view) {
+
+                isMute = !isMute;
+                if (isMute)
+                    volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_off_24);
+                else
+                    volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_up_24);
+
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean("isMute", isMute);
+                editor.apply();
+
             }
         });
 
-
     }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-    }
-
-
-
 }
